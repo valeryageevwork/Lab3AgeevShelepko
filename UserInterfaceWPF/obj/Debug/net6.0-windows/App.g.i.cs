82c8064cea0eb1768bf783682652defbd11811c4
valeryageevhome <valeryageevwork@yandex.ru>
2023-04-17 22:18:07 +0300

    first

--- UserInterfaceWPF/obj/Debug/net6.0-windows/App.g.i.cs
+++ UserInterfaceWPF/obj/Debug/net6.0-windows/App.g.i.cs
@@ -46,7 +46,7 @@ namespace UserInterfaceWPF {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -61,7 +61,7 @@ namespace UserInterfaceWPF {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
         public static void Main() {
             UserInterfaceWPF.App app = new UserInterfaceWPF.App();
             app.InitializeComponent();
